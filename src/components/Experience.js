db1d669f1066a2ae797d2e6bda7995d9d808a735
Preethi shyam <105586367+Pshyam17@users.noreply.github.com>
2026-02-11 04:22:30 -0800

    Remove job experience from Experience.js
    Removed job experience details for the Software Engineering Apprentice position at Nike.

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -25,12 +25,6 @@ class Experience extends React.Component {
             <span className="section-title">/ experience</span>
           </div>
           <JobList>
-
-Software Engineering Apprentice | Nike 					                                                         06/24 — 07/25 | 
-
-Implemented a LightGBM approach for the development of a product recommendation API for North American merchants, reducing the timeline of recommendation by ~3 months.
-Utilized NN clustering and decision trees for ground up feature importance analysis and selection for billions of evergreen North American products and LightGBM for forecasting.
-
       </JobList>
         </FadeInSection>
       </div>
